01766e0fe21945197a4accb44b4e19e54d207590
mihaimaganu17 <maganu_gx_mihai@yahoo.com>
2025-10-20 07:51:44 +0200

    Modified with proper comment style

--- react-newbie/ui_update/header.jsx
+++ react-newbie/ui_update/header.jsx
@@ -1,10 +1,10 @@
-// Select the div element with "some-div" id
+{/* Select the div element with "some-div" id */}
 const app = document.getElementById("some-div")
-// A component is a function that returns UI elements.
+{/* A component is a function that returns UI elements. */}
 function header() {
-    return <h1>Develop. Preview. Ship. </h1>;
+    return <h1>Develop. Preview. Ship. - from Flask </h1>;
 }
-// Create a react root node that will allow react to take over the DOM
+{/* Create a react root node that will allow react to take over the DOM */}
 const root = ReactDOM.createRoot(app);
-// Call render to display the React components inside the above node
-root.render(<h1>Develop.Preview.ship</h1>);
+{/* Call render to display the React components inside the above node */}
+root.render(<h1>Develop.Preview.ship3</h1>);
