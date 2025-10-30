ae1bc87e7e65369c6605f13adb688121c47f9323
mihaimaganu17 <maganu_gx_mihai@yahoo.com>
2025-10-30 10:56:08 +0100

    State challenge 4

---
+++ state_ch4/src/App.js
@@ -0,0 +1,13 @@
+export default function FeedbackForm() {
+
+  function handleClick() {
+    let name = prompt('What is your name?');
+    alert(`Hello, ${name}!`);
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Greet
+    </button>
+  );
+}
