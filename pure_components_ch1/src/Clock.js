7abc4a6c776f1e396ab79c60ea518de80b2fed8c
mihaimaganu17 <maganu_gx_mihai@yahoo.com>
2025-10-30 14:31:55 +0100

    Pure components challenges

---
+++ pure_components_ch1/src/Clock.js
@@ -0,0 +1,14 @@
+export default function Clock({ time }) {
+  const hours = time.getHours();
+  let className;
+  if (hours >= 0 && hours <= 6) {
+    className = 'night';
+  } else {
+    className = 'day';
+  }
+  return (
+    <h1 id="time" className={className}>
+      {time.toLocaleTimeString()}
+    </h1>
+  );
+}
