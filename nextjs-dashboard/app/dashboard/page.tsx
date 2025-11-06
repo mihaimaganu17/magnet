384471c507b7a35b625d1703a7ad787ccc5e1fc0
mihaimaganu17 <maganu_gx_mihai@yahoo.com>
2025-11-06 12:05:01 +0100

    Displaying invoices

--- nextjs-dashboard/app/dashboard/page.tsx
+++ nextjs-dashboard/app/dashboard/page.tsx
@@ -1,9 +1,11 @@
 import { lusitana_400 } from "@/app/ui/fonts";
-import { fetchRevenue } from "@/app/lib/data";
+import { fetchRevenue, fetchLatestInvoices } from "@/app/lib/data";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
+import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 
 export default async function Page() {
     const revenue = await fetchRevenue();
+    const invoices = await fetchLatestInvoices();
 
   return ( 
     <main>
@@ -19,6 +21,7 @@ export default async function Page() {
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
           <RevenueChart revenue={revenue} />
+          <LatestInvoices latestInvoices={invoices} />
       </div>
     </main>
   );
